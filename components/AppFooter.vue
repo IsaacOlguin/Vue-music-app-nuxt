f62ace2b78676facf190cc835564898a917e4cd7
IsaacOlguin <misael.olguin@gmail.com>
2020-07-16 15:52:35 -0500

    Proyecto basico app-music para verificar la funcionalidad de Nuxt

---
+++ components/AppFooter.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  footer.footer
+    .container
+        .content.has-text-centered
+            p Created by Isaac Olguin
+</template>
+
+<!--
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
+-->
---
+++ components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  section.hero.is-success
+    .hero-head
+        header.nav
+            .container
+                .nav-left
+                    .nav-item
+                        strong Music App &nbsp;
+                .nav-right.nav-menu
+                    //router-link.nav-item(v-bind:to=" {name: 'app-search' } ") Suchen &nbsp;
+                    router-link.nav-item(to="about") Wir &nbsp;
+    .hero-body
+        .container.has-text-centered
+            h1.title App Music
+            h2.subtitle Lists of songs  
+</template>
